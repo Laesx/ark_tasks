51b94b5fbd434241e5cfac25eb7ed2b964c2aa5e
Laesx <losguardianes15@gmail.com>
2024-04-30 13:26:21 +0200

    Better AI Subtask Prompt

--- lib/modules/home/home_provider.dart
+++ lib/modules/home/home_provider.dart
@@ -2,14 +2,14 @@ import 'package:flutter/material.dart';
 
 enum HomeTab {
   home,
-  students,
+  missions,
   tasks,
   schedule,
   settings;
 
   String get title => switch (this) {
         home => 'Home',
-        students => 'Students',
+        missions => 'Missions',
         tasks => 'Tasks',
         schedule => 'Schedule',
         settings => 'Settings',
@@ -17,7 +17,7 @@ enum HomeTab {
 
   IconData get iconData => switch (this) {
         home => Icons.home,
-        students => Icons.people_alt_outlined,
+        missions => Icons.people_alt_outlined,
         tasks => Icons.task_alt_outlined,
         schedule => Icons.schedule_outlined,
         settings => Icons.settings_outlined,
--- lib/modules/tasks/task_details.dart
+++ lib/modules/tasks/task_details.dart
@@ -4,10 +4,12 @@ import 'package:ark_jots/modules/tasks/task_card.dart';
 import 'package:ark_jots/modules/tasks/task_model.dart';
 import 'package:ark_jots/modules/tasks/task_providers.dart';
 import 'package:ark_jots/widgets/layouts/top_bar.dart';
+import 'package:choice/choice.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:ark_jots/utils/tools.dart';
+import 'package:async/async.dart';
 
 import '../../utils/ai_service.dart';
 
@@ -35,133 +37,81 @@ class _TaskDetailScreenState extends State<TaskDetailScreen> {
     final screenSize = MediaQuery.of(context).size;
     final xOffSet = screenSize.width * 0.25;
 
-    return PopScope(
-      onPopInvoked: (didPop) {
-        print("Popped");
-        if (didPop) {
-          //taskProvider.selectedTask = null;
-          if (task.title.isNotEmpty) {
-            taskProvider.updateOrCreateTask(task);
-            //taskProvider.deleteTask(task);
-            //task.save();
-          }
-        }
-      },
-      child: Scaffold(
-          appBar: TopBar(
-            trailing: [
-              IconButton(
-                icon: const Icon(Icons.bolt_outlined),
-                //onPressed: () => aiDialog(context, task)
-                onPressed: () => null,
-              )
-            ],
-            canPop: true,
-            //title: task.title,
-            title: "Task Details",
-          ),
-          body: Form(
-            child: ListView(
-              padding: const EdgeInsets.all(5),
-              children: [
-                //const SizedBox(height: 20),
-                Row(children: [
-                  TaskCheckbox(task),
-                  Flexible(
-                    child: TextFormField(
-                      style: const TextStyle(
-                        fontSize: 25,
-                      ),
-                      decoration: const InputDecoration(counterText: ''),
-                      minLines: 1,
-                      maxLines: 5,
-                      maxLength: 250,
-                      //maxLengthEnforcement: MaxLengthEnforcement.enforced,
-                      initialValue: task.title,
-                      onChanged: (value) => task.title = value,
-                      onTapOutside: (event) {
-                        FocusScopeNode currentFocus = FocusScope.of(context);
-                        if (!currentFocus.hasPrimaryFocus &&
-                            currentFocus.focusedChild != null) {
-                          currentFocus.focusedChild!.unfocus();
-                        }
-                      },
-                    ),
-                  ),
-                ]),
-                // Subtasks functionality
-                _SubtasksSection(
-                    widget: widget, task: task, controller: textController),
-
-                // AI Subtask Suggestions
-                ExpansionTile(
-                    title: const Text('AI Subtask Suggestions'),
-                    children: [
-                      _SubtastkSuggestions(task: task),
-                    ]),
-                // Due Date Menu
-                _DueDate(xOffSet: xOffSet, task: task),
-                const SizedBox(
-                  height: 20,
-                ),
-                // TODO: Reminders Menu
-                _Reminders(xOffSet: xOffSet, task: task),
-                TextFormField(
-                  initialValue: task.description,
-                  onChanged: (value) => task.description = value,
-                  decoration: const InputDecoration(labelText: 'Description'),
-                ),
-                // TODO: Make so this button is not necessary
-                ElevatedButton(
-                    onPressed: () => taskProvider.updateOrCreateTask(task),
-                    //onPressed: () => task.save(),
-                    child: const Text('Save'))
-              ],
-            ),
-          )),
-    );
-  }
-
-  /* Future<String?> aiDialog(BuildContext context, Task task) async {
-    // Returns a null Future
-    if (task.title.isEmpty) return Future(() => null);
-
-    Future<List<String>> response = AiService.getSubtasks(task.title);
-
-    // TODO: THIS NEEDS FIXING ONLY FOR TESTING!!!!
-    List<String> subtasks = await response;
-
-    return showDialog<String>(
-      context: context,
-      builder: (BuildContext context) => Dialog(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Wrap(
-                children: subtasks
-                    .asMap()
-                    .map((index, subtask) =>
-                        MapEntry(index, _MyButton(id: index, text: subtask)))
-                    .values
-                    .toList(),
-              ),
-              const Text('This is a typical dialog.'),
-              const SizedBox(height: 15),
-              TextButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: const Text('Close'),
-              ),
-            ],
-          ),
+    return Scaffold(
+        appBar: TopBar(
+          trailing: [
+            IconButton(
+              icon: const Icon(Icons.bolt_outlined),
+              //onPressed: () => aiDialog(context, task)
+              onPressed: () => null,
+            )
+          ],
+          canPop: true,
+          //title: task.title,
+          title: "Task Details",
         ),
-      ),
-    );
-  } */
+        body: Form(
+          child: ListView(
+            padding: const EdgeInsets.all(5),
+            children: [
+              //const SizedBox(height: 20),
+              Row(children: [
+                TaskCheckbox(task),
+                Flexible(
+                  child: TextFormField(
+                    style: const TextStyle(
+                      fontSize: 25,
+                    ),
+                    decoration: const InputDecoration(counterText: ''),
+                    minLines: 1,
+                    maxLines: 5,
+                    maxLength: 250,
+                    //maxLengthEnforcement: MaxLengthEnforcement.enforced,
+                    initialValue: task.title,
+                    onChanged: (value) => task.title = value,
+                    onTapOutside: (event) {
+                      FocusScopeNode currentFocus = FocusScope.of(context);
+                      if (!currentFocus.hasPrimaryFocus &&
+                          currentFocus.focusedChild != null) {
+                        currentFocus.focusedChild!.unfocus();
+                      }
+                    },
+                  ),
+                ),
+              ]),
+              // Subtasks functionality
+              _SubtasksSection(
+                  widget: widget, task: task, controller: textController),
+
+              // AI Subtask Suggestions
+              /* ExpansionTile(
+                  title: const Text('AI Subtask Suggestions'),
+                  children: [
+                    _SubtastkSuggestions(task: task),
+                  ]), */
+
+              _SubtaskPrompt(),
+              // Due Date Menu
+              _DueDate(xOffSet: xOffSet, task: task),
+              const SizedBox(
+                height: 20,
+              ),
+              // TODO: Reminders Menu
+              _Reminders(xOffSet: xOffSet, task: task),
+              TextFormField(
+                initialValue: task.description,
+                onChanged: (value) => task.description = value,
+                decoration: const InputDecoration(labelText: 'Description'),
+              ),
+              // TODO: Make so this button is not necessary
+              ElevatedButton(
+                  onPressed: () => taskProvider.updateOrCreateTask(task),
+                  //onPressed: () => task.save(),
+                  child: const Text('Save'))
+            ],
+          ),
+        ));
+  }
 }
 
 class _Reminders extends StatefulWidget {
@@ -382,6 +332,81 @@ class _SubtasksSectionState extends State<_SubtasksSection> {
   }
 }
 
+class _SubtaskPrompt extends StatefulWidget {
+  const _SubtaskPrompt({super.key});
+
+  @override
+  State<_SubtaskPrompt> createState() => _SubtaskPromptState();
+}
+
+class _SubtaskPromptState extends State<_SubtaskPrompt> {
+  List<String> choicesValue = [];
+  final choicesMemoizer = AsyncMemoizer<List<String>>();
+
+  void setChoicesValue(List<String> value) {
+    //print(value);
+    setState(() => choicesValue = value);
+  }
+
+  Future<List<String>> getChoices() async {
+    return AiService.getSubtasks("Levantarme de la Cama");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<List<String>>(
+        initialData: const [],
+        future: choicesMemoizer.runOnce(getChoices),
+        builder: (context, snapshot) {
+          return SizedBox(
+            width: 300,
+            child: Card(
+              child: PromptedChoice<String>.multiple(
+                confirmation: true,
+                title: 'AI Subtasks',
+                clearable: true,
+                error: snapshot.hasError,
+                errorBuilder: ChoiceListError.create(
+                  message: snapshot.error.toString(),
+                ),
+                loading: snapshot.connectionState == ConnectionState.waiting,
+                value: choicesValue,
+                onChanged: setChoicesValue,
+                itemCount: snapshot.data?.length ?? 0,
+                itemBuilder: (state, i) {
+                  final choice = snapshot.data?.elementAt(i);
+                  return CheckboxListTile(
+                    value: state.selected(choice!),
+                    onChanged: state.onSelected(choice),
+                    title: Text(choice),
+                  );
+                },
+                modalHeaderBuilder: ChoiceModal.createHeader(
+                  title: const Text('Select Subtasks'),
+                  automaticallyImplyLeading: false,
+                  actionsBuilder: [
+                    // (state) {
+                    //   final values = snapshot.data!;
+                    //   return Checkbox(
+                    //     value: state.selectedMany(values),
+                    //     onChanged: state.onSelectedMany(values),
+                    //     tristate: true,
+                    //   );
+                    // },
+                    ChoiceModal.createConfirmButton(),
+                    ChoiceModal.createSpacer(width: 10),
+                    // ChoiceModal.createSpacer(width: 25),
+                  ],
+                ),
+                promptDelegate: ChoicePrompt.delegateBottomSheet(),
+                anchorBuilder: ChoiceAnchor.create(valueTruncate: 1),
+              ),
+            ),
+          );
+        });
+  }
+}
+
 class _SubtastkSuggestions extends StatelessWidget {
   const _SubtastkSuggestions({super.key, required this.task});
 
@@ -391,6 +416,8 @@ class _SubtastkSuggestions extends StatelessWidget {
   Widget build(BuildContext context) {
     List<String> subtasks = [];
 
+    //List<String> multipleSelected = [];
+
     return Scrollable(
       viewportBuilder: (context, offset) {
         return FutureBuilder<List<String>>(
@@ -398,20 +425,83 @@ class _SubtastkSuggestions extends StatelessWidget {
           builder: (context, snapshot) {
             if (snapshot.connectionState == ConnectionState.done) {
               subtasks = snapshot.data ?? [];
-              return Wrap(
-                children: subtasks
-                    .asMap()
-                    .map((index, subtask) =>
-                        MapEntry(index, _MyButton(id: index, text: subtask)))
-                    .values
-                    .toList(),
+
+              return PromptedChoice.multiple(
+                itemCount: subtasks.length,
+                itemBuilder: (context, index) {
+                  return ChoiceChip(
+                    label: Text(subtasks[index]),
+                    selected: context.selected(subtasks[index]),
+                    onSelected: context.onSelected(subtasks[index]),
+                  );
+                },
               );
+
+              //return _SubtaskSelect(subtasks: subtasks);
+
+              // return Wrap(
+              //   children: subtasks
+              //       .asMap()
+              //       .map((index, subtask) =>
+              //           MapEntry(index, _MyButton(id: index, text: subtask)))
+              //       .values
+              //       .toList(),
+              // );
             } else {
               return const Center(child: CircularProgressIndicator());
             }
           },
         );
       },
+    );
+  }
+}
+
+class _SubtaskSelect extends StatefulWidget {
+  const _SubtaskSelect({
+    super.key,
+    required this.subtasks,
+  });
+
+  final List<String> subtasks;
+
+  @override
+  State<_SubtaskSelect> createState() => _SubtaskSelectState();
+}
+
+class _SubtaskSelectState extends State<_SubtaskSelect> {
+  List<String> multipleSelected = [];
+
+  void setMultipleSelected(List<String> value) {
+    setState(() => multipleSelected = value);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InlineChoice.multiple(
+      itemCount: widget.subtasks.length,
+      clearable: true,
+      onChanged: setMultipleSelected,
+      value: multipleSelected,
+      itemBuilder: (context, index) {
+        return ChoiceChip(
+          label: Text(widget.subtasks[index]),
+          selected: context.selected(widget.subtasks[index]),
+          onSelected: context.onSelected(widget.subtasks[index]),
+        );
+      },
+
+      listBuilder: ChoiceList.createWrapped(
+        direction: Axis.horizontal,
+        spacing: 10,
+        runSpacing: 0,
+      ),
+
+      // listBuilder: ChoiceList.createGrid(
+      //   //direction: Axis.horizontal,
+      //   //physics: ClampingScrollPhysics(),
+      //   //spacing: 10,
+      // ),
     );
   }
 }
